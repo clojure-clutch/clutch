3e3ce7fa2a7fb1b112560a465a3ccd0acbf86c1a
Chas Emerick <cemerick@snowtide.com>
2010-08-20 01:12:02 -0400

    -> clojure 1.2.0 final, -> 0.2.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject com.ashafa/clutch "0.2.2"
+(defproject com.ashafa/clutch "0.2.3-SNAPSHOT"
   :description "A Clojure library for Apache CouchDB."
   :main com.ashafa.clutch.view-server
-  :dependencies [[org.clojure/clojure "1.2.0-RC3"]
-                 [org.clojure/clojure-contrib "1.2.0-RC3"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[autodoc "0.7.0"]
                      [swank-clojure "1.2.1"]
                      [lein-clojars "0.5.0-SNAPSHOT"]])
