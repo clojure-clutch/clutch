788a62a91805f79fb2a4ac56893d7c85db9e1387
Chas Emerick <cemerick@snowtide.com>
2011-08-20 00:33:12 -0400

    add test configuration for clojure 1.2.0

--- project.clj
+++ project.clj
@@ -4,7 +4,11 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojure/data.json "0.1.1"]
                  [org.clojure/tools.logging "0.1.2"]]
-  :dev-dependencies [[autodoc "0.9.0-SNAPSHOT"]
-                     [swank-clojure "1.2.1"]
-		                 [lein-run "1.0.0"]
-                     [lein-clojars "0.5.0-SNAPSHOT"]])
+  :dev-dependencies [#_[autodoc "0.9.0-SNAPSHOT"]  ;; commented deps because they were pulling in clojure 1.1.0...
+                     #_[swank-clojure "1.2.1"]
+                     #_[lein-clojars "0.5.0-SNAPSHOT"]
+                     [lein-multi "1.1.0-SNAPSHOT"]]
+  :multi-deps {"clojure-1.2.0" [[org.clojure/clojure "1.2.0"]
+                                [org.clojure/clojure-contrib "1.2.0"]
+                                [org.clojure/data.json "0.1.1"]
+                                [org.clojure/tools.logging "0.1.2"]]})
