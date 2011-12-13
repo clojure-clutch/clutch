4af1cda4890d41201df0428c310dad4f1aa13d18
Chas Emerick <cemerick@snowtide.com>
2011-12-12 19:53:16 -0500

    doc tweak, be explicit about requiring Clojure 1.2.0+

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
 (defproject com.ashafa/clutch "0.3.0-SNAPSHOT"
   :description "A Clojure library for Apache CouchDB."
   :url "http://github.com/ashafa/clutch"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "[1.2.0,)"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojure/data.json "0.1.2"]
                  [org.clojure/tools.logging "0.1.2"]]
   :dev-dependencies [[swank-clojure "1.3.3"]
                      [lein-clojars "0.7.0"]
                      [lein-multi "1.1.0-SNAPSHOT"]]
-  :multi-deps {"clojure-1.2.0" [[org.clojure/clojure "1.2.0"]
+  :multi-deps {"clojure-1.3.0" [[org.clojure/clojure "1.3.0"]
                                 [org.clojure/clojure-contrib "1.2.0"]
-                                [org.clojure/data.json "0.1.1"]
+                                [org.clojure/data.json "0.1.2"]
                                 [org.clojure/tools.logging "0.1.2"]]}
   :test-selectors {:default (constantly true)
                    :no-encoding #(not= 'test-docid-encoding (:name %))})
