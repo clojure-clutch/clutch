f32adffac2eea2e6160cc3579b0d29f176ec7959
Chas Emerick <chas@cemerick.com>
2012-04-09 10:05:47 -0400

    require lein2 for builds; update travis build

--- project.clj
+++ project.clj
@@ -1,18 +1,16 @@
 (defproject com.ashafa/clutch "0.3.1-SNAPSHOT"
   :description "A Clojure library for Apache CouchDB."
   :url "http://github.com/ashafa/clutch"
-  :dependencies [[org.clojure/clojure "[1.2.0,)"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojure/data.json "0.1.2"]
                  [org.clojure/tools.logging "0.1.2"]
                  [commons-codec "1.6"]
                  [com.cemerick/url "0.0.3"]]
-  :dev-dependencies [[swank-clojure "1.3.3"]
-                     [lein-clojars "0.7.0"]
-                     [lein-multi "1.1.0-SNAPSHOT"]]
-  :multi-deps {"clojure-1.3.0" [[org.clojure/clojure "1.3.0"]
-                                [org.clojure/clojure-contrib "1.2.0"]
-                                [org.clojure/data.json "0.1.2"]
-                                [org.clojure/tools.logging "0.1.2"]]}
+  :profiles {:dev {:plugins [[lein-clojars "0.8.0"]]}
+             :1.2 {:dependencies [[org.clojure/clojure "1.2.0"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0-beta5"]]}}
+  :aliases  { "all" ["with-profile" "dev,1.2:dev:dev,1.4"] }
+  :min-lein-version "2.0.0"
   :test-selectors {:default (constantly true)
                    :no-encoding #(not= 'test-docid-encoding (:name %))})
